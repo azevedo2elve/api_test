1a13f0a987aa96133cc724b2fc70fa2ede67117e
azevedo2elve <azevedogabriel00@gmail.com>
2024-07-12 22:38:35 -0300

    formating with pint

--- app/Http/Controllers/Cliente/StoreController.php
+++ app/Http/Controllers/Cliente/StoreController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Cliente\StoreRequest;
 use App\Models\Cliente;
 use Illuminate\Http\Request;
+
 class StoreController extends Controller
 {
     /**
@@ -14,9 +15,9 @@ class StoreController extends Controller
     public function __invoke(StoreRequest $request)
     {
         Cliente::create([
-            'nome' => $request->request->get('nome'),
+            'nome'     => $request->request->get('nome'),
             'telefone' => $request->request->get('telefone'),
-            'email' => $request->request->get('email'),
+            'email'    => $request->request->get('email'),
         ]);
     }
 }
--- tests/Feature/CreateClienteTest.php
+++ tests/Feature/CreateClienteTest.php
@@ -1,13 +1,12 @@
 <?php
 
-use function Pest\Laravel\assertDatabaseHas;
-use function Pest\Laravel\postJson;
+use function Pest\Laravel\{assertDatabaseHas, postJson};
 
 it('can create a cliente', function () {
     $data = [
-        'nome' => 'Joe Doe',
+        'nome'     => 'Joe Doe',
         'telefone' => '55555555',
-        'email' => 'joe@doe.com',
+        'email'    => 'joe@doe.com',
     ];
 
     postJson(route('clientes.store'), $data)->assertSuccessful();
