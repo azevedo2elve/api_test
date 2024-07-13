1a031e16e5502251a15bfb6ff4dcc769c9993ee8
azevedo2elve <azevedogabriel00@gmail.com>
2024-07-12 22:25:14 -0300

    wip

--- tests/Feature/CreateClienteTest.php
+++ tests/Feature/CreateClienteTest.php
@@ -4,15 +4,13 @@ use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
 
 it('can create a cliente', function () {
-    $data = postJson(route('clientes.store'), [
+    $data = [
         'nome' => 'Joe Doe',
         'telefone' => '55555555',
         'email' => 'joe@doe.com',
-    ])->assertSuccessful();
+    ];
 
-    assertDatabaseHas('clientes', [
-        'nome' => 'Joe Doe',
-        'telefone' => '55555555',
-        'email' => 'joe@doe.com',
-    ]);
+    postJson(route('clientes.store'), $data)->assertSuccessful();
+
+    assertDatabaseHas('clientes', $data);
 });
